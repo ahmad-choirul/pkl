3178394208616a06dc77df8a9050971ac544757a
ahmad-choirul <31815400+ahmad-choirul@users.noreply.github.com>
2018-11-11 15:17:57 +0700

    update
    tambahsurat

--- application/controllers/suratkemenag/Csuratdinas.php
+++ application/controllers/suratkemenag/Csuratdinas.php
@@ -3,8 +3,8 @@
 
 class Csuratdinas extends CI_Controller {
 	function __construct() 
-    {
-        parent::__construct();
+	{
+		parent::__construct();
 
 		// benchmark aja...
 		// if (1==2) 
@@ -19,11 +19,11 @@ class Csuratdinas extends CI_Controller {
 		// }
 
 		$this->data = null;
-	    $logged_in = $this->session->userdata('logged_in');
-	    if(!isset($logged_in['id_rules']))
-	    {
-	    	redirect('/suratkemenag/auth/logout/');
-	    }
+		$logged_in = $this->session->userdata('logged_in');
+		if(!isset($logged_in['id_rules']))
+		{
+			redirect('/suratkemenag/auth/logout/');
+		}
 		$this->login = $this->session->userdata('logged_in');
 	}
 
@@ -45,7 +45,7 @@ class Csuratdinas extends CI_Controller {
 		//inputan dan editan
 		$crud->fields('kode_devisi','nama','nip','pangkatgol','jabatan','tingkat_biaya','dasar','maksud','alat_angkut','tempat_berangkat','tempat_tujuan','tanggal_awal','tanggal_akhir','tempat','tanggal_pembuatan');
 		$crud->add_fields('kode_devisi','nama','nip','pangkatgol','jabatan','tingkat_biaya','dasar','maksud','alat_angkut','tempat_berangkat','tempat_tujuan','tanggal_awal','tanggal_akhir','tempat','atas_nama');
-$crud->edit_fields('kode_devisi','nama','nip','pangkatgol','jabatan','tingkat_biaya','dasar','maksud','alat_angkut','tempat_berangkat','tempat_tujuan','tanggal_awal','tanggal_akhir','tempat','atas_nama');
+		$crud->edit_fields('kode_devisi','nama','nip','pangkatgol','jabatan','tingkat_biaya','dasar','maksud','alat_angkut','tempat_berangkat','tempat_tujuan','tanggal_awal','tanggal_akhir','tempat','atas_nama');
 
 		//ganti nama kolom
 		$crud->display_as('id','ID');
@@ -62,7 +62,7 @@ $crud->edit_fields('kode_devisi','nama','nip','pangkatgol','jabatan','tingkat_bi
 		$crud->display_as('id_user','Perekam');
 		$crud->display_as('file_suratmasuk','File surat masuk');
 		$crud->field_type('atas_nama','dropdown',
-array('Kepala kemenag' => 'Kepala kemenag', 'Kasubbag' => 'Kasubbag'));
+			array('Kepala kemenag' => 'Kepala kemenag', 'Kasubbag' => 'Kasubbag'));
 		$crud->set_relation('kode_devisi','m_kodeklasifikasi','nama_klasifikasi');
 
 		//kolom yang ditampilkan di table
@@ -76,6 +76,7 @@ array('Kepala kemenag' => 'Kepala kemenag', 'Kasubbag' => 'Kasubbag'));
 		$crud->unset_delete();
 
 		$crud->add_action('Cetak', base_url().'tt.png', 'suratkemenag/Csuratdinas/cetak');
+		$crud->add_action('Cetak2', base_url().'disp.png', 'suratkemenag/Csuratdinas/cetak2');
 
 		$output = $crud->render();
 
@@ -115,27 +116,24 @@ array('Kepala kemenag' => 'Kepala kemenag', 'Kasubbag' => 'Kasubbag'));
 		$docx->set('jabatan',$surat['jabatan']);
 		$docx->set('tempat',$surat['tempat']);
 		$docx->set('untuk',$surat['maksud']);
-		$docx->set('tgl_awal',$surat['tanggal_awal']);
-		$docx->set('tgl_akhir',$surat['tanggal_akhir']);
+		$docx->set('tgl_awal',$surat['tanggal_awal']);//
+		$docx->set('tgl_akhir',$surat['tanggal_akhir']);//
 		if ($surat['atas_nama']=='Kasubbag') {
 			$docx->set('atas_nama',' a.n Kepala
-			   		         Kepala Sub Bagian Tata Usaha
+				Kepala Sub Bagian Tata Usaha
 
 
 
-Machfudz
-');
+				Machfudz
+				');
 		}else if ($surat['atas_nama']=='Kepala kemenag') {
 			$docx->set('atas_nama','Kepala Kementerian agama
 
 
 
-Machfudz
-');
+				Machfudz
+				');
 		}
-		
-
-
 		$docx->set('tgl_sekarang',$this->tgl_indo($tanggal));
 
 		$docx->downloadAs ('surat.docx');
@@ -143,7 +141,56 @@ Machfudz
 		header("Content-Type:application/vnd.openxmlformats-officedocument.wordprocessingml.document");
 		header("Content-Disposition: attachment;filename=surat.docx");
 	}
+	function rubahtanggal($tgl)
+	{
+		if (($timestamp = strtotime($tgl)) !== false)
+		{  
+			$tanggal = date('Y-m-d', $timestamp);  
+		}
+		return $this->tgl_indo($tanggal);
+	}
+	function cetak2($id)
+	{
+		include 'docxtemplate.class.php';
+		$this->load->model('Msuratdinas');
+		//$this->load->library('docxtemplate');
+		// $data = $this->Msuratdinas->getSurat($id);
+		// printf($data);
+		$surat = $this->Msuratdinas->getSurat($id);
+		$str = $surat['tanggal_pembuatan'];
+		if (($timestamp = strtotime($str)) !== false)
+		{
+			$php_date = getdate($timestamp);
+			$bulan = date("m", $timestamp);
+			$tahun = date("Y", $timestamp);  
+			$tanggal = date('Y-m-d', $timestamp);  
+		}
 
+		$docx = new DOCXTemplate('spd.docx');
+		$docx->set('nomor_surat',$surat['id']);
+		$docx->set('nama_pembuat','dari session');//
+		$docx->set('bulan',$bulan);//
+		$docx->set('tahun',$tahun);//
+		$docx->set('nama',$surat['nama']);//
+		$docx->set('untuk',$surat['maksud']);//
+		$docx->set('nip',$surat['nip']);//
+		$docx->set('alat_angkut',$surat['alat_angkut']);//
+		$docx->set('total_hari','masih belum');//
+		$docx->set('tempat_berangkat',$surat['tempat_berangkat']);//
+		$docx->set('tempat_tujuan',$surat['tempat_tujuan']);//
+		$docx->set('jabatan',$surat['jabatan']);//
+		$docx->set('tanggal_awal',$surat['tanggal_awal']);//
+		$docx->set('tanggal_akhir',$surat['tanggal_akhir']);//
+		$docx->set('tanggal_awal',$this->rubahtanggal($surat['tanggal_awal']));
+		$docx->set('tanggal_akhir',$this->rubahtanggal($surat['tanggal_akhir']));
+
+		$docx->set('tanggal_pembuatan',$this->tgl_indo($tanggal));
+
+		$docx->downloadAs ('surat2.docx');
+
+		header("Content-Type:application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+		header("Content-Disposition: attachment;filename=surat2.docx");
+	}
 	function tgl_indo($tanggal){
 		$bulan = array (
 			1 =>   'Januari',
@@ -164,7 +211,7 @@ Machfudz
 		// variabel pecahkan 0 = tanggal
 		// variabel pecahkan 1 = bulan
 		// variabel pecahkan 2 = tahun
-	 
+
 		return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
 	}
 }
